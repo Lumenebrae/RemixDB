c415e3b362abb19dfc1363d3171bb4e5f6ccd3f9
Kevin Xue <lumenebrae@gmail.com>
2020-12-05 14:15:04 -0500

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -10,5 +10,5 @@
 
 CREATE INDEX idx_albums_name ON albums (Name(255), ALID) USING BTREE;
 CREATE INDEX idx_artists_name ON artists (Name(255), AID) USING BTREE;
-CREATE INDEX idx_groups_name ON `groups` (Name(255), GID) USING BTREE;
+CREATE INDEX idx_groups_name ON bandgroups (Name(255), GID) USING BTREE;
 CREATE INDEX idx_track_name ON track (Name(255), TID) USING BTREE;
