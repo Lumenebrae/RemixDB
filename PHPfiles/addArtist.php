eeae0b1e823167fcdb1537d13eb3a47996164126
Lumenebrae <lumenebrae@gmail.com>
2020-12-07 16:29:21 -0500

    more ironing and link to viewtrack

--- PHPfiles/addArtist.php
+++ PHPfiles/addArtist.php
@@ -4,7 +4,6 @@
 $q = $_REQUEST["q"];
 $inputArray = explode("_", $q);
 
-
 $con = mysqli_connect('127.0.0.1', "newuser", '', 'cs348');
 if (!$con) {
     die('Not connected : ' . mysqli_connect_error());
@@ -24,12 +23,15 @@ try {
     $row = @mysqli_fetch_assoc($result);
     mysqli_commit($con);
 
-    //send the trackID back
+    //send the artistID back
     header('Access-Control-Allow-Origin: *');
     header("Content-type: text/xml");
-    echo '<id>';
+    echo "<?xml version='1.0' ?>";
+    echo '<info>';
+    echo '<id ';
     echo 'id="' . $row['id'] . '" ';
-    echo '</id>';
+    echo '/>';
+    echo '</info>';
 
 } catch (mysqli_sql_exception $exception){
     mysqli_rollback($con);
