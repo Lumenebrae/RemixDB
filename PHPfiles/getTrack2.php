594f9a2a01467af26ecfd3b148fd96220095fa10
Thomas Fitzgibbons <tfitzgi@purdue.edu>
2020-12-07 18:46:15 -0500

    updates to orm library

--- PHPfiles/getTrack2.php
+++ PHPfiles/getTrack2.php
@@ -1,37 +1,13 @@
 <?php
+include 'orm.php';
 $q = $_REQUEST["q"];
 
-$connection=mysqli_connect ('127.0.0.1', "newuser", '', 'cs349');
-if (!$connection) {
-    die('Not connected : ' . mysqli_connect_error());
-}
-//retrieve track data
-$query = "select * from track where TID=".$q;
-$resultT = mysqli_query($connection, $query);
-if (!$resultT) { #does this differentiate between null and empty? the answer: it does not.
-    die('Invalid query: ' . mysqli_error($connection));
-}
-
-//retrieve associated artists
-$query = "select * from artists where AID in (select AID from trackcreditartist where TID=".$q.")";
-$resultA = mysqli_query($connection, $query);
-
-//retrieve associated groups
-$query = "select * from bandgroups where GID in (select GID from trackcreditgroup where TID=".$q.")";
-$resultG = mysqli_query($connection, $query);
-
-$query = "select * from albums where ALID in (select ALID from albumtracks where TID=".$q.")";
-$resultAL = mysqli_query($connection, $query);
-
-//retrieve associated remixes. Should use a join later to access transformation type and artist
-$query = "select * from track where TID in (select TransfromedID from remix where OriginalID=".$q.")";
-$resultR = mysqli_query($connection, $query);
-
-//retrieve associated sources. Should use a join later to access transformation type and artist
-$query = "select * from track where TID in (select OriginalID from remix where TransfromedID=".$q.")";
-$resultS = mysqli_query($connection, $query);
-
-$row = @mysqli_fetch_assoc($resultT);
+$resultA = getTrackCreditArtist($q);
+$resultG = getTrackCreditGroup($q);
+$resultAL = getTrackCreditAlbum($q);
+$resultR = getTrackCreditRemix($q);
+$resultS = getTrackCreditSource($q);
+$resultT = getTrack($q);
 
 header('Access-Control-Allow-Origin: *');
 header("Content-type: text/xml");
@@ -39,69 +15,70 @@ header("Content-type: text/xml");
 echo "<?xml version='1.0' ?>";
 echo '<info>';
 echo '<track ';
-echo 'title="' . $row['Name'] . '" ';
-echo 'length="' . $row['Length'] . '" ';
-echo 'genre="' . $row['Genre'] . '" ';
-echo 'UID="' . $row['UID'] . '" ';
+echo 'title="' . $resultT['Name'] . '" ';
+echo 'length="' . $resultT['Length'] . '" ';
+echo 'genre="' . $resultT['Genre'] . '" ';
+echo 'UID="' . $resultT['UID'] . '" ';
 echo '/>';
 
-
-if ($resultA) {
-    while ($row = @mysqli_fetch_assoc($resultA)){
+if (!empty($resultA)) {
+    $index = 0;
+    while (!empty($resultA[$index])){
         // Add to XML document node
         echo '<artist ';
-        echo 'AID="' . $row['AID'] . '" ';
-        echo 'artistName="' . $row['Name'] . '" ';
+        echo 'AID="' . $resultA[$index]['AID'] . '" ';
+        echo 'artistName="' . $resultA[$index]['Name'] . '" ';
         echo '/>';
+        $index++;
     }
 }
 
-//iterate through groups
-if ($resultG) {
-    while ($row = @mysqli_fetch_assoc($resultG)){
-        // Add to XML document node
+if (!empty($resultG)) {
+    $index = 0;
+    while (!empty($resultG[$index])){
         echo '<group ';
-        echo 'GID="' . $row['GID'] . '" ';
-        echo 'name="' . $row['Name'] . '" ';
-        echo 'year="' . $row['YearFormed'] . '" ';
-        echo 'type="' . $row['type'] . '" ';
+        echo 'GID="' . $resultG[$index]['GID'] . '" ';
+        echo 'name="' . $resultG[$index]['Name'] . '" ';
+        echo 'year="' . $resultG[$index]['YearFormed'] . '" ';
+        echo 'type="' . $resultG[$index]['type'] . '" ';
         echo '/>';
+        $index++;
     }
 }
 
-if ($resultAL) {
-    while ($row = @mysqli_fetch_assoc($resultAL)){
-        // Add to XML document node
+if (!empty($resultAL)) {
+    $index = 0;
+    while (!empty($resultAL[$index])){
         echo '<album ';
-        echo 'ALID="' . $row['ALID'] . '" ';
-        echo 'name="' . $row['Name'] . '" ';
+        echo 'ALID="' . $resultAL[$index]['ALID'] . '" ';
+        echo 'name="' . $resultAL[$index]['Name'] . '" ';
         echo '/>';
+        $index++;
     }
 }
 
-//iterate through remixes
-if ($resultR) {
-    while ($row = @mysqli_fetch_assoc($resultR)){
-        // Add to XML document node
+if (!empty($resultR)) {
+    $index = 0;
+    while (!empty($resultR[$index])){
         echo '<remix ';
-        echo 'TID="' . $row['TID'] . '" ';
-        echo 'title="' . $row['Name'] . '" ';
+        echo 'TID="' . $resultR[$index]['TID'] . '" ';
+        echo 'title="' . $resultR[$index]['Name'] . '" ';
         echo '/>';
+        $index++;
     }
 }
 
-//iterate through sources
-if ($resultS) {
-    while ($row = @mysqli_fetch_assoc($resultS)){
-        // Add to XML document node
+if (!empty($resultS)) {
+    $index = 0;
+    while (!empty($resultS[$index])){
         echo '<source ';
-        echo 'TID="' . $row['TID'] . '" ';
-        echo 'title="' . $row['Name'] . '" ';
+        echo 'TID="' . $resultS[$index]['TID'] . '" ';
+        echo 'title="' . $resultS[$index]['Name'] . '" ';
         echo '/>';
+        $index++;
     }
 }
 
-// End XML file
 echo '</info>';
-$connection->close();
+
 
--- PHPfiles/orm.php
+++ PHPfiles/orm.php
@@ -59,3 +59,93 @@ function getTrack($q){ //function parameters, two variables.
     endcon($connection);
     return @mysqli_fetch_assoc($result);
 }
+function getTrackCreditArtist($q){
+    $connection = init();
+    $query = "select * from artists where AID in (select AID from trackcreditartist where TID=".$q.")";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        die('Invalid query: ' . mysqli_error($connection));
+    }
+    $result2 = array();
+    $index = 0;
+    while ($row = @mysqli_fetch_assoc($result)){
+        // Add to XML document node
+        $result2[] = $row;
+        $index++;
+    }
+
+    endcon($connection);
+    return $result2;
+}
+
+function getTrackCreditGroup($q){
+    $connection = init();
+    $query = "select * from bandgroups where GID in (select GID from trackcreditgroup where TID=".$q.")";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        die('Invalid query: ' . mysqli_error($connection));
+    }
+    $result2 = array();
+    $index = 0;
+    while ($row = @mysqli_fetch_assoc($result)){
+        // Add to XML document node
+        $result2[] = $row;
+        $index++;
+    }
+    endcon($connection);
+    return $result2;
+}
+
+function getTrackCreditAlbum($q){
+    $connection = init();
+    $query = "select * from albums where ALID in (select ALID from albumtracks where TID=".$q.")";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        die('Invalid query: ' . mysqli_error($connection));
+    }
+    $result2 = array();
+    $index = 0;
+    while ($row = @mysqli_fetch_assoc($result)){
+        // Add to XML document node
+        $result2[] = $row;
+        $index++;
+    }
+    endcon($connection);
+    return $result2;
+}
+
+function getTrackCreditRemix($q){
+    $connection = init();
+    $query = "select * from track where TID in (select TransformedID from remix where OriginalID=".$q.")";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        die('Invalid query: ' . mysqli_error($connection));
+    }
+    $result2 = array();
+    $index = 0;
+    while ($row = @mysqli_fetch_assoc($result)){
+        // Add to XML document node
+        $result2[] = $row;
+        $index++;
+    }
+    endcon($connection);
+    return $result2;
+}
+
+function getTrackCreditSource($q){
+    $connection = init();
+    $query = "select * from track where TID in (select OriginalID from remix where TransformedID=".$q.")";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        die('Invalid query: ' . mysqli_error($connection));
+    }
+    $result2 = array();
+    $index = 0;
+    while ($row = @mysqli_fetch_assoc($result)){
+        // Add to XML document node
+        $result2[] = $row;
+        $index++;
+    }
+    endcon($connection);
+    return $result2;
+}
